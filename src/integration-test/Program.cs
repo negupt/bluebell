f569c29a4dd1928cdec44d563e5e3a139002e694
bartr <bartr@outlook.com>
2019-09-22 01:19:49 -0500

    added tinyweb

--- src/integration-test/Program.cs
+++ src/integration-test/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HeliumIntegrationTest
@@ -11,10 +13,43 @@ namespace HeliumIntegrationTest
         public static async Task Main(string[] args)
         {
             int sleepMs = 0;
+            int threads = 1;
+            bool runWeb = false;
             string baseUrl = "http://localhost:4120";
             string defaultInputFile = "integration-test.json";
             bool loop = false;
             List<string> fileList = new List<string>();
+
+            // Get environment variables
+            string env = Environment.GetEnvironmentVariable("RUNWEB");
+            if (!string.IsNullOrEmpty(env))
+            {
+                bool.TryParse(env, out runWeb);
+            }
+
+            env = Environment.GetEnvironmentVariable("THREADS");
+            if (!string.IsNullOrEmpty(env))
+            {
+                int.TryParse(env, out threads);
+            }
+
+            env = Environment.GetEnvironmentVariable("HOST");
+            if (!string.IsNullOrEmpty(env))
+            {
+                baseUrl = env;
+            }
+
+            env = Environment.GetEnvironmentVariable("SLEEP");
+            if (!string.IsNullOrEmpty(env))
+            {
+                int.TryParse(env, out sleepMs);
+            }
+
+            env = Environment.GetEnvironmentVariable("THREADS");
+            if (!string.IsNullOrEmpty(env))
+            {
+                int.TryParse(env, out threads);
+            }
 
             // process the command line args
             if (args.Length > 0)
@@ -31,27 +66,20 @@ namespace HeliumIntegrationTest
 
                 while (i < args.Length)
                 {
+                    // handle web (-w)
+                    if (args[i] == "-w")
+                    {
+                        runWeb = true;
+                    }
+
+
                     // process all of the args in pairs
-                    if (i < args.Length - 1)
+                    else if (i < args.Length - 1)
                     {
                         // handle host (-h http://localhost:4120/)
                         if (args[i] == "-h")
                         {
                             baseUrl = args[i + 1].Trim();
-
-                            // make it easier to pass host
-                            if (! baseUrl.ToLower().StartsWith("http"))
-                            {
-                                if (baseUrl.ToLower().StartsWith("localhost"))
-                                {
-                                    baseUrl = "http://" + baseUrl;
-                                }
-                                else
-                                {
-                                    baseUrl = string.Format("https://{0}.azurewebsites.net", baseUrl);
-                                }
-                            }
-
                             i++;
                         }
 
@@ -91,19 +119,58 @@ namespace HeliumIntegrationTest
                             }
                         }
 
-                        i++;
+                        // handle threads (-t numberThreads)
+                        else if (args[i] == "-t")
+                        {
+                            if (int.TryParse(args[i + 1], out threads) && threads > 0 && threads <= 10)
+                            {
+                                i++;
+                            }
+                            else
+                            {
+                                // exit on error
+                                Console.WriteLine("Invalid number of threads paramter: {0}\r\n", args[i + 1]);
+                                Usage();
+                                return;
+                            }
+                        }
                     }
+
+                    i++;
                 }
             }
 
-            if (fileList.Count == 0)
+            // make it easier to pass host
+            if (!baseUrl.ToLower().StartsWith("http"))
             {
-                fileList.Add(defaultInputFile);
+                if (baseUrl.ToLower().StartsWith("localhost"))
+                {
+                    baseUrl = "http://" + baseUrl;
+                }
+                else
+                {
+                    baseUrl = string.Format("https://{0}.azurewebsites.net", baseUrl);
+                }
+            }
+
+            if (sleepMs < 0)
+            {
+                sleepMs = 0;
+            }
+
+            if (threads < 1)
+            {
+                threads = 1;
+            }
+
+            if (threads > 10)
+            {
+                threads = 10;
             }
 
             Smoker.Test smoker = new Smoker.Test(fileList, baseUrl);
 
-            if (! loop)
+            if (!loop && ! runWeb)
             {
                 if (!smoker.Run().Result)
                 {
@@ -113,15 +180,38 @@ namespace HeliumIntegrationTest
                 return;
             }
 
-            await smoker.RunLoop(sleepMs);
+            if (runWeb)
+            {
+                // start the web server
+                Tiny.Web host = new Tiny.Web();
 
+                List<Task> tasks = new List<Task>();
+
+                if (loop)
+                {
+                    for (int i = 0; i < threads; i++)
+                    {
+                        tasks.Add(smoker.RunLoop(sleepMs));
+                    }
+                }
+
+                await host.RunAsync(16, 4120);
+
+                return;
+            }
+
+            if (fileList.Count == 0)
+            {
+                fileList.Add(defaultInputFile);
+            }
+
+            await smoker.RunLoop(sleepMs);
         }
 
         // display the usage text
         private static void Usage()
         {
-            Console.WriteLine("Usage: integration-test [-i file1.json [file2.json] [file3.json] ...] [-h hostUrl] [-s sleepMS] [--help]");
+            Console.WriteLine("Usage: integration-test [-i file1.json [file2.json] [file3.json] ...] [-h hostUrl] [-s sleepMS] [-t numberOfThreads] [-w] [--help]");
         }
-
     }
 }
---
+++ src/integration-test/TinyWeb.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tiny
+{
+    class Web
+    {
+
+        private readonly HttpListener listener = new HttpListener();
+        private Semaphore sem;
+
+        public async Task RunAsync(int listenCount, int port)
+        {
+            string prefix = string.Format("http://*:{0}/", port);
+
+            listener.IgnoreWriteExceptions = true;
+
+            // Add the server bindings:
+            Console.WriteLine("TinyWeb Listening on {0} ...", prefix);
+            listener.Prefixes.Add(prefix);
+
+            listener.Start();
+
+            sem = new Semaphore(listenCount, listenCount, "HttpListenCount");
+            int requestId = 0;
+
+            // Open and keep open listenCount listeners
+            while (true)
+            {
+                sem.WaitOne();
+                requestId++;
+
+                await StartListener(requestId);
+            }
+        }
+
+        private async Task StartListener(int requestId)
+        {
+            // Wait for a request context
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            // Allow a new listener to be set up
+            sem.Release();
+
+            // Log request to console
+            //Console.WriteLine("{0}: {1}", requestId, context.Request.RawUrl);
+
+            // process the current request
+            // replace this with your own request handler
+            await UnderConstruction(context);
+        }
+
+
+        // Echo the raw URL as a plain text response
+        private async Task UnderConstruction(HttpListenerContext context)
+        {
+            await Task.Run(() =>
+            {
+                const string message = "Under construction ...";
+
+                try
+                {
+                    // Handle request here - every URL returns message as plain text
+                    context.Response.ContentType = "text/plain";
+
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(message);
+                    context.Response.ContentLength64 = buffer.Length;
+                    context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                    context.Response.OutputStream.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+    }
+}
