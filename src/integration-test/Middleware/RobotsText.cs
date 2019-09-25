3396df45939faf8deb3fbd8cc17a9a50081f696f
bartr <bartr@outlook.com>
2019-09-25 18:57:50 -0500

    updated comments

--- src/integration-test/Middleware/RobotsText.cs
+++ src/integration-test/Middleware/RobotsText.cs
@@ -4,6 +4,9 @@ namespace HeliumIntegrationTest
 {
     public static class RobotsMiddlewareExtensions
     {
+        // response bytes
+        static readonly byte[] b = System.Text.Encoding.UTF8.GetBytes("User-agent: *\r\nDisallow: /\r\n");
+
         /// <summary>
         /// Middleware extension method to handle /robots*.txt request
         /// App Service sends a warmup request of /robots8327.txt (where 8327 is a random number)
@@ -18,22 +21,21 @@ namespace HeliumIntegrationTest
             builder.Use(async (context, next) =>
             {
                 // convert to lower and remove the leading /
-                string path = context.Request.Path.Value.ToLower().Substring(1);
+                string path = context.Request.Path.Value.ToLower().Trim().Substring(1);
 
                 // matches /robots*.txt (/robots.txt /robots123.txt etc)
                 // does not match /robots/robots.txt
-                if (path.StartsWith("robots") && path.EndsWith(".txt") && !path.Contains("/"))
+                if (path.StartsWith("robots") && 
+                    path.EndsWith(".txt") && 
+                    ! path.Contains("/"))
                 {
-                    // convert content to byte array
-                    byte[] b = System.Text.Encoding.UTF8.GetBytes("User-agent: *\r\nDisallow: /\r\n");
-
                     // return the content
                     context.Response.ContentType = "text/plain";
                     await context.Response.Body.WriteAsync(b, 0, b.Length);
                 }
                 else
                 {
-                    // not a match, so call next middleware handler
+                    // not a match, so call next middleware
                     await next();
                 }
             });
--- src/integration-test/Startup.cs
+++ src/integration-test/Startup.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace HeliumIntegrationTest
 {
     public class Startup
     {
+        // standard aspnet core startup except for last two lines
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -42,6 +37,8 @@ namespace HeliumIntegrationTest
             }
 
             app.UseMvc();
+
+            // add our custom middleware
             app.UseRobots();
             app.UseHomePage();
         }
