ae71ceab6a5f91cbed69e74c7322d91f2a951f64
bartr <bartr@outlook.com>
2019-09-25 21:26:34 -0500

    added config class

---
+++ src/integration-test/config.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HeliumIntegrationTest
+{
+    public class Config
+    {
+        public int SleepMs = 0;
+        public int Threads = 0;
+        public bool RunLoop = false;
+        public bool RunWeb = false;
+        public string Host = "http://localhost:4120";
+        public readonly List<string> FileList = new List<string>();
+    }
+}
