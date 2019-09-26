d422fea6f1f21ab94c8ff1ceca3eda5dfb5f5c3d
bartr <bartr@outlook.com>
2019-09-26 00:23:28 -0500

    added /api/runtest controller

---
+++ src/integration-test/Controllers/RunTestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeliumIntegrationTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RunTestController : Controller
+    {
+        [HttpGet()]
+        public async Task<string> Runtest()
+        {
+            Response.ContentType = "text/plain";
+
+            return await App.smoker.RunOnce(App.tokens.Count);
+        }
+    }
+}
