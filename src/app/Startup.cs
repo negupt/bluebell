d690e364696885b61139f7972cb47657387af40f
bartr <bartr@outlook.com>
2019-09-29 04:03:38 -0500

    removed using

--- src/app/Startup.cs
+++ src/app/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
-using Helium;
 
 namespace Helium
 {
